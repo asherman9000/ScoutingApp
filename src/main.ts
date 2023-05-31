6cedffafe2a1517757b0caac9c3dff3ca8abf7e9
BecauseISaid <67928852+asherman9000@users.noreply.github.com>
2023-05-30 18:19:06 -0700

    Grid setup in CSS

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
+class pane {
+    private name: string;
+    public constructor(name: string) {
+        this.name = name;
+    }
+
+    public makePane(id: HTMLElement) : void {
+        id.innerHTML = "class='div red'";
+    }
+}
 let id = document.getElementById("id")!;
 
 let p = new pane("hi");
 p.makePane(id);
+
